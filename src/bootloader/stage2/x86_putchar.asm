9ee6e79e6a3adaabcc51b6f8fbf44d3c7ec3e7a3
Nagendra Kumar Jamadagni <nagendrajamadagni@gmail.com>
2024-07-27 16:57:01 -0500

    Commented putchar assembly code

--- src/bootloader/stage2/x86_putchar.asm
+++ src/bootloader/stage2/x86_putchar.asm
@@ -5,12 +5,13 @@ section _TEXT class=CODE
 global _x86_PutCharToScreen
 
 _x86_PutCharToScreen:
-    push bp             ; Save old bp and move it to sp
+    push bp             ; Save old bp and move sp to bp
     mov bp, sp          ; New call frame
 
     push bx             ; Save callee saved bx register
 
-    ; [bp + 2] - Callee saved value of BX
+    ; [bp - 2] - Callee saved value of BX
+    ; [bp + 0] - Old value of BP
     ; [bp + 2] - Return address (RIP)
     ; [bp + 4] - First argument the character to be printed as a 16 bit word.
     ; [bp + 6] - Second argument the page
@@ -22,6 +23,6 @@ _x86_PutCharToScreen:
     int 0x10
 
     pop bx              ; Restore callee saved register
-    mov sp, bp
-    pop bp
+    mov sp, bp          ; Restore old sp
+    pop bp              ; Restore old bp
     ret
